4d9eb11fa4aedad3e543adf3adf0dd29f46684f3
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-06 21:45:12 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,15 +1,12 @@
 class Cat
 attr_accessor :name
-<<<<<<< HEAD
+
 def meow
 puts "meow!"
 end
 end
 maru = Cat.new     ## code your solution here.
 maru.meow
-=======
 
-end
-maru = Cat.new 
-puts "meow"  ## code your solution here.
->>>>>>> 6719cc4ab5ed547c37f4e92c78d90354aab7e251
+
+
